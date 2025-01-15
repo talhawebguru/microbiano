443a58051688809f5e0d7c117f9eae50f05ca9dc
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2025-01-15 17:07:03 +0500

    product list design also change

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
         <SliderHero />
         <Card />
-        <ProductSection/>
+        {/* <ProductSection/> */}
         <AboutUsSection />
         <Vision />
         <Banner />
